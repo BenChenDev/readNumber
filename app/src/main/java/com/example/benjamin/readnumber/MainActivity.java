1fe5de1603a369964afc5570567d3312da4745f6
TRUZijunChen <benjamin9527@icloud.com>
2019-02-13 13:43:28 -0800

    added opencv module

--- app/src/main/java/com/example/benjamin/readnumber/MainActivity.java
+++ app/src/main/java/com/example/benjamin/readnumber/MainActivity.java
@@ -1,27 +1,45 @@
 package com.example.benjamin.readnumber;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.benjamin.readnumber.utils.CommonUtils;
+import com.example.benjamin.readnumber.views.TouchImageView;
+
 
 import static com.example.benjamin.readnumber.utils.CommonUtils.info;
 
 public class MainActivity extends AppCompatActivity {
 
     Button takePic;
-
+    EditText recognizeResult;
+    static ProcessImage processImg = new ProcessImage();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private String lastFileName = "";
+    private boolean isRecognized = false;
+    private TouchImageView image;
+
+    ProgressDialog progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         takePic = findViewById(R.id.takePicButton);
-
+        recognizeResult = findViewById(R.id.recognize_result);
+        image = findViewById(R.id.grid_img);
         takePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,10 +49,112 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void takePicture(){
-        Intent camera = new Intent(MainActivity.this, AndroidCamera.class);
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         lastFileName = CommonUtils.APP_PATH + "capture" + System.currentTimeMillis() + ".jpg";
         camera.putExtra("output", lastFileName);
         info(lastFileName);
-        startActivityForResult(camera, 1);
+        if (camera.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(camera, REQUEST_IMAGE_CAPTURE);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            Bitmap imageBitmap = BitmapFactory.decodeFile(lastFileName, options);
+
+            if (imageBitmap == null) {
+                // Try again
+                isRecognized = false;
+                image.setImageBitmap(imageBitmap);
+                hideProcessBar();
+                dialogBox("Can not recognize sheet. Please try again", "Retry", "Exist", true);
+                return;
+            }
+            final Bitmap finalImageBitmap = imageBitmap.getWidth() > imageBitmap.getHeight()
+                    ? rotateBitmap(imageBitmap, 90) : imageBitmap;
+
+            int top = data.getIntExtra("top", 0);
+            int bot = data.getIntExtra("bot", 0);
+            int right = data.getIntExtra("right", 0);
+            int left = data.getIntExtra("left", 0);
+
+            image.setImageBitmap(finalImageBitmap);
+            displayResult(finalImageBitmap, top, bot, right, left);
+
+        }
+    }
+
+    public void hideProcessBar() {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (progressBar != null && progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+        });
+    }
+
+    public void displayResult(Bitmap imageBitmap, int top, int bot, int right, int left) {
+        info("Origin size: " + imageBitmap.getWidth() + ":" + imageBitmap.getHeight());
+        // Parser
+        recognizeResult.setText("");
+        if (processImg.parseBitmap(imageBitmap, top, bot, right, left)) {
+            // TODO: set result
+            recognizeResult.setText(processImg.recognizeResult);
+            // TODO: write result to image
+            // image.setImageBitmap(toBitmap(processImg.drawAnswered(numberAnswer)));
+            isRecognized = true;
+            hideProcessBar();
+        } else {
+            // Try again
+            isRecognized = false;
+            image.setImageBitmap(imageBitmap);
+            hideProcessBar();
+            dialogBox("Can not recognize sheet. Please try again", "Retry", "Exist", true);
+        }
+    }
+
+    public Bitmap rotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    public void dialogBox(String message, String bt1, String bt2, final boolean flagContinue) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.setPositiveButton(bt1, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                if (flagContinue) {
+                    takePicture();
+                }
+            }
+        });
+
+        if (bt2 != "") {
+            alertDialogBuilder.setNegativeButton(bt2, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    existApp();
+                    // return false;
+                }
+            });
+        }
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void existApp() {
+        CommonUtils.cleanFolder();
+        this.finish();
+    }
+
 }
--- app/src/main/java/com/example/benjamin/readnumber/utils/CommonUtils.java
+++ app/src/main/java/com/example/benjamin/readnumber/utils/CommonUtils.java
@@ -3,11 +3,34 @@ package com.example.benjamin.readnumber.utils;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.File;
+
 public class CommonUtils {
     public static String TAG = "COMPA";
     public static String APP_PATH = Environment.getExternalStorageDirectory() + "/RecognizeTextOCR/";
 
     public static void info(Object msg) {
         Log.i(TAG, msg.toString());
+    }
+
+    /**
+     * Clear temp image in folder APP_PATH
+     */
+    public static void cleanFolder() {
+        info("Create or empty folder");
+        String datapath = APP_PATH;
+        File tenpPath = new File(datapath);
+        if (!tenpPath.exists()) {
+            if (!tenpPath.mkdir()) {
+                // Can not create path
+            }
+        } else {
+            for (File child : tenpPath.listFiles()) {
+                // Keep only config files
+                if (!child.getName().contains(".txt")) {
+                    child.delete();
+                }
+            }
+        }
     }
 }
